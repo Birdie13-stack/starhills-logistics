d83ae1a2cccf492f24aa4bfcad5106322a9ed957
Chinenye <birdievsq@gmail.com>
2025-11-22 14:18:32 +0100

    Hero section animation

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -24,12 +24,12 @@ export default function Hero() {
   };
 
   const slideVariants = {
-    enter: (direction) => ({
+    enter: (direction: number) => ({
       x: direction > 0 ? "100%" : "-100%",
       opacity: 0,
     }),
     center: { x: "0%", opacity: 1 },
-    exit: (direction) => ({
+    exit: (direction: number) => ({
       x: direction < 0 ? "100%" : "-100%",
       opacity: 0,
     }),
